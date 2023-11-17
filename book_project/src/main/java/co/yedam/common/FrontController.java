99a82a15da2d49f942494063a5c7ad0362c2cbc8
민 <148922864+min00020@users.noreply.github.com>
2023-11-17 18:05:09 +0900

    1117 디벨롭 수정중

--- book_project/src/main/java/co/yedam/common/FrontController.java
+++ book_project/src/main/java/co/yedam/common/FrontController.java
@@ -43,7 +43,6 @@ import co.yedam.user.web.JoinFormControl;
 import co.yedam.user.web.LoginControl;
 import co.yedam.user.web.LoginFormControl;
 import co.yedam.user.web.LogoutControl;
-import co.yedam.user.web.ModifyFormControl;
 import co.yedam.user.web.ModifyUserControl;
 import co.yedam.user.web.MypageControl;
 
@@ -99,7 +98,7 @@ public class FrontController extends HttpServlet {
 		map.put("/idCheck.do", new IdCheckControl()); /* 아이디 중복 확인 */
 
 		map.put("/modifyuser.do", new ModifyUserControl()); /* 회원 정보 수정 처리 */
-		map.put("/modifyForm.do", new ModifyFormControl()); /* 회원 정보 수정 화면 마이페이지 생성하면 필요한지 모르겠음*/
+		map.put("/modifyForm.do", new co.yedam.user.web.ModifyFormControl()); /* 회원 정보 수정 화면 마이페이지 생성하면 필요한지 모르겠음*/
 		map.put("/mypage.do", new MypageControl()); /* 마이 페이지 */
 		
 		//관리자
